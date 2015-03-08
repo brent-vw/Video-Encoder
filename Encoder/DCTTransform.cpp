4129d36946ab813c1b72b0c0fee4cf18363ed6b9
Brent Van Wynsberge <Brent.VanWynsberge@UGent.be>
2015-03-08 14:50:18 +0100

    DCTTransform implemented

--- Encoder/DCTTransform.cpp
+++ Encoder/DCTTransform.cpp
@@ -8,7 +8,15 @@
 // Initialiseren van de matrix A (zie DCTTransform.h). Maak hiervoor gebruik van de formule 3 in de opgave.
 DCTTransform::DCTTransform()
 {
-	// ... //
+	double C;
+	int N = 8;
+	for(int i=0; i<N, i++){
+		for(int j=0; j<N; j++){
+			C = sqrt(1/(double)N);
+			if(i>0) C = sqrt(2/(double)N);
+			A[i][j] = C*cos(((2*j+1)*i*M_PI)/(2*N));
+		}
+	}
 }
 
 void DCTTransform::Transform(Macroblock *mb)
@@ -30,7 +38,27 @@ void DCTTransform::Transform(Macroblock *mb)
 // Uitvoeren van de DCT op een 8x8 blok. De offsets worden gebruikt om de plaats van de luminantiecoefficienten aan te duiden.
 void DCTTransform::Transform_8x8(pixel **block, int offset_x, int offset_y)
 {
-	// ... //
+	//A*X
+	int AX[8][8];
+	for(int i=0; i<8; i++){
+		for(int j=0; j<8; j++){
+			for(int k=0; k<8; k++){
+				AX[i][j] += A[i][k] * block[k+offset_x][j+offset_y];
+			}
+		}
+	}
+
+	//(A*X)*A^T
+	for(int i=0; i<8; i++){
+		for(int j=0; j<8; j++){
+			//Waarden op 0 zetten voor hergebruik
+			block[i+offset_x][j+offset_y] = 0;
+			for(int k=0; k<8; k++){
+				// A[i][j] = (A[j][i])^T
+				block[i+offset_x][j+offset_y] = AX[i][k] * A[j][k];
+			}
+		}
+	}
 }
 
 
